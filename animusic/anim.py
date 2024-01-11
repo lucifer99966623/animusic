3d6aa5d8ae6a16c3bef8b10a7f6268bba03f4358
lucifer99966623 <156064275+lucifer99966623@users.noreply.github.com>
2024-01-10 21:43:08 -0500

    Update anim.py
    it wasnt handling audio an video paths seperatly

--- animusic/anim.py
+++ animusic/anim.py
@@ -1,38 +1,145 @@
-import librosa
-import numpy as np
-import pandas as pd
-import os
-import matplotlib.pyplot as plt
-import PIL
-import mimetypes
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
-from moviepy import editor
-from functools import partial
-from tqdm import tqdm
-from . import effects
+def create_animation(img=None, audio=None, video=None, output='animusic.mp4', start_time=0, end_time=None, fps=30, frame_smoothing=3, visualizer=False):
+    if isinstance(start_time, str):
+        start_time = mins_to_secs(start_time)
+    if isinstance(end_time, str):
+        end_time = mins_to_secs(end_time)
+        
+    # global original_image
+    # original_image = PIL.Image.open(img)
 
+    if mimetypes.guess_type(img)[0].startswith('video'):
+        video = img
+        img = None
+    
+    print('Analyzing audio...')
+    
+    if video:
+        video_clip = editor.VideoFileClip(video)
+        fps = video_clip.fps
+        end_time = start_time + video_clip.duration
+    elif audio:
+        y, sr = librosa.load(audio, sr=None)
+        if end_time is None:
+            end_time = librosa.get_duration(y, sr)
+        duration = end_time - start_time
+        video_clip = editor.ImageClip(img, duration=duration).set_fps(fps)
+        y = y[int(start_time*sr):int(end_time*sr)]
+    else:
+        # Handle the case when both audio and video are None
+        raise ValueError("Either 'audio' or 'video' must be provided.")
+    
+    frame_times = []
+    for t, frame in video_clip.iter_frames(with_times=True):
+        frame_times.append(t)
 
-def mins_to_secs(time_str):
-    mins, secs = time_str.split(':')
-    mins = int(mins) * 60
-    secs = int(secs)
-    return mins + secs
+    hop_length = int(np.round(sr/fps))
+    win_length = int(np.round(frame_smoothing*sr/fps))
+    n_fft = int(2**np.ceil(np.log2(win_length)))
+    
+    yh, yp = librosa.effects.hpss(y, margin=1)
+    S = librosa.feature.melspectrogram(yp, sr=sr, power=1, n_fft=n_fft, hop_length=hop_length, win_length=win_length, window='blackman', center=False)
+#     S = librosa.decompose.nn_filter(S, aggregate=np.median)
+    rec = librosa.segment.recurrence_matrix(S, mode='affinity', metric='cosine', sparse=True)
+    S = librosa.decompose.nn_filter(S, rec=rec, aggregate=np.average)
 
+    
+    components, activations = librosa.decompose.decompose(S, 2, sort=True)
+#     bin_idx = np.argsort(components, axis=0)[components.shape[0] // 2]
+#     activations = activations[np.argsort(bin_idx)]
+    # low, mid, high = activations
+    low, high = activations
+    
+    times = librosa.times_like(S, sr=sr, n_fft=n_fft, hop_length=hop_length)
+    signals = pd.DataFrame({
+        'low': low,
+        # 'mid': mid,
+        'high': high
+    }, index=times)
+#     signals = np.sqrt(signals)
+    signals = (signals - signals.min()) / (signals.max() - signals.min())
+#     signals /= signals.max()
+#     min_cutoff = 0.25
+#     max_cutoff = 0.45
+#     signals = signals.clip(min_cutoff, max_cutoff)
+#     signals = (signals - min_cutoff) / (max_cutoff - min_cutoff)
+        
+#     onset_env = librosa.onset.onset_strength(sr=sr, S=S)
+#     beats = librosa.onset.onset_detect(sr=sr, onset_envelope=onset_env, units='time')
+    signals = signals.reindex(frame_times, method='nearest')
+#     signals = signals.rolling(6, win_type='blackman', center=True).mean().fillna(0)
+#     signals = signals.clip(0)
+    # signals.plot()
+    # plt.show()
+    signals = (signals - signals.min()) / (signals.max() - signals.min())
+#     signals /= signals.max()
 
-def secs_to_mins(secs):
-    mins, secs = secs // 60, secs % 60
-    return f'{mins:01d}:{secs:02d}'
+#     original_image = np.array(Image.open(img).convert('RGB'))
+#     frames = []
+#     for i, t in enumerate(tqdm(frame_times)):
+# #         suspect_frames = [
+# #             719,
+# # #             838
+# #         ]
+# #         if i not in suspect_frames:
+# #             continue
+# #         print(i)30
+#         frame = original_image.copy()
+# #         print(frame.shape)
+#         frame = apply_effect(frame, t, effect=sin_wave_distortion, signals=signals.high)
+# #         print(frame.shape)
+#         frame = apply_effect(frame, t, effect=chromatic_aberration, signals=signals.high)
+#         frame = apply_effect(frame, t, effect=zoom, signals=signals.low)
+#         frames.append(frame)
+# #         if frame.shape != (998, 998, 3):
+# #             print(i, t, frame.shape)
 
+#     video_clip = ImageSequenceClip(frames, fps=fps)
+#     audio_clip = AudioFileClip(audio, fps=sr).subclip(t_start=start_time, t_end=end_time)
+#     video_clip = video_clip.set_audio(audio_clip)
+        
+#     Effects
+    video_clip = (video_clip
+        .fl(partial(
+            apply_effect,
+            effect=effects.chromatic_aberration,
+            signals=signals.high,
+        ))
+        .fl(partial(
+            apply_effect,
+            effect=effects.sin_wave_distortion,
+            signals=signals.high,
+        ))
+        .fl(partial(
+            apply_effect,
+            effect=effects.zoom,
+            signals=signals.low,
+        ))
+    )
+    
+    if visualizer:
+        print('Drawing visualizer...')
 
-def draw_visualizer(S_frame, height=1000, width=1000, dpi=100):
-    fig = Figure(figsize=(width/dpi, height/dpi), dpi=dpi)
-    canvas = FigureCanvas(fig)
+        bars = 64
+        S = librosa.feature.melspectrogram(y, sr=sr, power=0.5, n_mels=bars, n_fft=n_fft, hop_length=hop_length, win_length=win_length, window='blackman')
+        S = (S - S.min()) / (S.max() - S.min())
 
-    ax = fig.gca()
-    ax.axis('off')
-    fig.tight_layout()
-    fig.patch.set_alpha(0)
+        visualizer_frames = []
+        for S_frame in tqdm(S.T):
+            frame = draw_visualizer(S_frame, height=video_clip.h, width=video_clip.w)
+            visualizer_frames.append(frame)
+
+        visualizer_clip = editor.ImageSequenceClip(visualizer_frames, fps=fps)
+        video_clip = editor.CompositeVideoClip([video_clip, visualizer_clip])
+    
+    print('Rendering video...')
+    # video_clip.preview()
+    video_clip.write_videofile(output,
+                               audio_codec='aac',
+                               threads=os.cpu_count(),
+                               preset='medium',
+#                                preset='veryslow',
+                               ffmpeg_params=['-bf', '2', '-b_strategy', '2']
+                              )
 
     ax.set_ylim([0, 1])
     ax.bar(np.arange(S_frame.shape[0]), S_frame*0.2, color='#ab20fd')
